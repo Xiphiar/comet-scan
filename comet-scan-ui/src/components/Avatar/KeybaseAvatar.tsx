9aedbd716487aa3e22ecfc4c26371fe84c31bf5e
Xiphiar <me@xiphiar.com>
2025-03-10 22:18:01 -0400

    Fallback to circle if validator image is broken

--- comet-scan-ui/src/components/Avatar/KeybaseAvatar.tsx
+++ comet-scan-ui/src/components/Avatar/KeybaseAvatar.tsx
@@ -11,13 +11,17 @@ type Props = {
 
 const ValidatorAvatar: FC<Props> = ({ avatarUrl, moniker, size = '40px' }) => {
     const color = stringToColor(moniker);
+    const filteredMoniker = moniker?.replace(/[^0-9a-zA-Z]/gi, '');
+    const firstLetter = filteredMoniker?.charAt(0).toUpperCase();
     
     return (
         <div className={styles.avatarWrapper} style={{height: size, width: size}}>
             { avatarUrl ?
-                <img src={avatarUrl} />
+                <div style={{backgroundColor: color}}>
+                    <img src={avatarUrl} alt={firstLetter} />
+                </div>
             :
-                <div style={{backgroundColor: color}}>{moniker?.charAt(0).toUpperCase()}</div>
+                <div style={{backgroundColor: color}}>{firstLetter}</div>
             }
         </div>
     )
